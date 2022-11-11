8d93d53fa417947a5de7fa7710aa70d31ef7adc8
krishvadhani19 <krish.vadhani@somaiya.edu>
2022-11-11 10:58:00 +0530

    some changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,8 @@ import React from "react";
 const Home = () => {
   return (
     <>
-      <div className="h-full bg-slate-400">This is Home</div>;
+      <div className="h-full bg-slate-400">
+        This is Home</div>;
     </>
   );
 };
