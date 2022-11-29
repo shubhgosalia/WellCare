1985858fb14a0ea108f81a4dec8e511cbb2e2194
krishvadhani19 <krish.vadhani@somaiya.edu>
2022-11-29 15:25:14 +0530

    left arrow

---
+++ src/components/Icons/Light/leftArrow.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function HomeLine({
+  size = 18, // or any default size of your choice
+  color = "black", // or any color of your choice
+}) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 512 512"
+      width={size} // added size here
+      height={size} // added size here
+      fill={color} // added color here
+    >
+      <path d="M109.3 288L480 288c17.7 0 32-14.3 32-32s-14.3-32-32-32l-370.7 0 73.4-73.4c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-128 128c-12.5 12.5-12.5 32.8 0 45.3l128 128c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.3 288z" />
+    </svg>
+  );
+}
