3448fba901d188312294b75903c56cd6c8a5fbca
krishvadhani19 <krish.vadhani@somaiya.edu>
2022-11-29 16:27:51 +0530

    Login and Sign up Done

--- src/App.js
+++ src/App.js
@@ -1,31 +1,9 @@
 import "./App.css";
-// import Navbar from "./components/Navbar";
-// import Hero from "./components/Hero";
-// import About from "./components/About";
-// import Service from "./components/Services";
-// import Footer from "./components/Footer";
-import Login from "components/Login";
+import Login from "components/Authentication/Login";
 import Home from "components/Home/Home";
 import { Routes, Route } from "react-router-dom";
 import { BrowserRouter as Router } from "react-router-dom";
-import Signup from "components/Signup";
-// import Navbar from "./components/Navbar";
-// import Hero from "./components/Hero";
-// import About from "./components/About";
-// import Service from "./components/Services";
-// import Footer from "./components/Footer";
-// import Reset from "./components/Reset";
-// import Forgot from "./components/Forgot";
-
-// import { useState } from "react";
-// import Stepper from "./components/Stepper";
-// import StepperControl from "./components/StepperControl";
-// import { UseContextProvider } from "./contexts/StepperContext";
-
-// import Step1 from "./components/steps/Step1";
-// import Step2 from "./components/steps/Step2";
-// import Step3 from "./components/steps/Step3";
-// import Final from "./components/steps/Final";
+import Signup from "components/Authentication/Signup";
 
 function App() {
   // const [currentStep, setCurrentStep] = useState(1);
