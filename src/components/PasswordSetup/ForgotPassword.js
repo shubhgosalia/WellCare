3567be864f0736a4a3247dd15e6f20d3ba87ea28
shubhgosalia <shubh604@gmail.com>
2023-01-21 14:16:27 +0530

    some changes

--- src/components/PasswordSetup/ForgotPassword.js
+++ src/components/PasswordSetup/ForgotPassword.js
@@ -11,11 +11,11 @@ const ForgotPassword = () => {
         <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
           <div class="w-full p-6 bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md dark:bg-gray-800 dark:border-gray-700 sm:p-8">
             <h2 class="mb-3 text-xl font-bold leading-tight tracking-tight text-blue-600 md:text-2xl dark:text-white">
-              Password Reset
+              Forgot Password ?
             </h2>
             <p class="mb-2">
-              Please enter your e-mail address and we will send you a link to
-              reset your password!
+              Don't worry, please enter your e-mail address and we will send you
+              a link to reset your password!
             </p>
             <form class="mt-4 space-y-4 lg:mt-5 md:space-y-5" action="#">
               {
