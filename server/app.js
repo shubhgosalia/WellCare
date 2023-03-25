a63d9473dc2e05d7ac8eacd02aba7c51de0e20e2
krishvadhani19 <126052973+krishcloudworx@users.noreply.github.com>
2023-03-25 15:09:18 +0530

    some changes in backend

--- server/app.js
+++ server/app.js
@@ -19,30 +19,30 @@ const server = app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);
 });
 
-//initialized the socket 
+//initialized the socket
 const io = socket(server, {
   cors: {
     origin: "http://localhost:3000",
     credentials: true,
   },
-})
+});
 
 //list of online users
 global.onlineusers = new Map();
 
 //creating socket connection
-io.on("connection", (socket) => {
-  console.log("new connection : ", socket.id);
-  socket.on("add-user", (userId) => {
-    onlineusers.set(userId, socket.id);
-  })
+// io.on("connection", (socket) => {
+//   console.log("new connection : ", socket.id);
+//   socket.on("add-user", (userId) => {
+//     onlineusers.set(userId, socket.id);
+//   })
 
-  socket.on("send-msg", (data) => {
-    // To find receiver users socket
-    const findSocketUser = onlineusers.get(data.to);
-    if (findSocketUser) {
-      //emitting the message to a particular client
-      socket.to(findSocketUser).emit("msg-receive", data.message);
-    }
-  })
-});
+//   socket.on("send-msg", (data) => {
+//     // To find receiver users socket
+//     const findSocketUser = onlineusers.get(data.to);
+//     if (findSocketUser) {
+//       //emitting the message to a particular client
+//       socket.to(findSocketUser).emit("msg-receive", data.message);
+//     }
+//   })
+// });
