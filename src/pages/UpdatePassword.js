0f348d0719bd6a7b9ffa5eb96faf349b6f9d3840
krishvadhani19 <krish.vadhani@somaiya.edu>
2023-04-14 19:29:53 +0530

    Update password page shifted

--- src/pages/UpdatePassword.js
+++ src/pages/UpdatePassword.js
@@ -1,24 +1,9 @@
-import React, { useState } from "react";
+import React from "react";
 import Navbar from "components/Utils/Navbar";
 import Header from "components/UpdatePassword/Header";
-
-// icons
-import FullEyeIcon from "components/Icons/Bold/fullEye";
-import EyeOffIcon from "components/Icons/Bold/eyeOff";
+import Form from "components/UpdatePassword/Form";
 
 const UpdatePassword = () => {
-  //
-  const [passwordVisiblity, setPasswordVisiblity] = useState(false);
-
-  const handlePassword = () => {
-    if (passwordVisiblity) {
-      setPasswordVisiblity(false);
-    } else {
-      setPasswordVisiblity(true);
-    }
-  };
-
-  let password = "Krish2001@";
   return (
     <div className="w-full flex flex-row bg-gradient-to-r from-dark-100 via-dark-200 to-dark-100 font-body-primary">
       {/* Navbar */}
@@ -34,50 +19,7 @@ const UpdatePassword = () => {
 
         {/* Form */}
         <div className="my-20 w-2/3 ml-16 text-white">
-          {/* current Password */}
-          <div className="flex w-full justify-between bg-dark-100 rounded-lg  p-5 border border-dark-200">
-            {/* Label and Input */}
-            <div className="w-[90%] flex flex-col">
-              {/* label */}
-              <div className="text-base text-dark-600">Password</div>
-
-              {/* input */}
-              {passwordVisiblity ? (
-                <div className="py-1 bg-dark-100  outline-none text-xl">
-                  {password}
-                </div>
-              ) : (
-                <input
-                  type="password"
-                  id="password"
-                  name="password"
-                  placeholder="Enter Password"
-                  minLength={1}
-                  autoComplete="do-not-autofill"
-                  className="py-1 bg-dark-100  outline-none text-xl"
-                ></input>
-              )}
-            </div>
-
-            {/* Icon */}
-            {passwordVisiblity ? (
-              <div className="my-auto" onClick={handlePassword}>
-                <EyeOffIcon size="28" color="#ffffff" />
-              </div>
-            ) : (
-              <div className="my-auto" onClick={handlePassword}>
-                <FullEyeIcon size="28" color="#ffffff" />
-              </div>
-            )}
-          </div>
-
-          {/* new password */}
-          <div></div>
-
-          {/* confirm new password */}
-          <div></div>
-
-          {/* save changes and cancel*/}
+          <Form />
         </div>
       </div>
     </div>
